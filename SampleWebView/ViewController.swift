71a11e4d4673f5e438292805824267e50809f7f4
choco0908 <starfish0908@naver.com>
2020-10-22 21:50:41 +0900

    Google Webiview 생성 커밋

--- SampleWebView/ViewController.swift
+++ SampleWebView/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webviewMain: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //1. url string
+        
+        let urlString = "https://www.google.com"
+        
+        if let url = URL(string: urlString) {
+            let urlRequest = URLRequest(url: url)
+            webviewMain.load(urlRequest)
+        }
+        //2. url > request
+        
+        
+        
+        //3. req > load
+        
+        
     }
 
 
