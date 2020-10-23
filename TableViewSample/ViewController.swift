a2aca9700c7be7a8860bc9f0c17a665fdd4b6d40
choco0908 <starfish0908@naver.com>
2020-10-23 22:02:14 +0900

    TableView Sample 연습

--- TableViewSample/ViewController.swift
+++ TableViewSample/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var mainTableview: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //1. 임의의 셀 만들기
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        
+        
+        cell.textLabel?.text = "TEST\(indexPath.row)"
+        
+        return cell
+        
+        //2.
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainTableview.delegate = self
+        mainTableview.dataSource = self
     }
 
-
+    //Tableview
+    //정갈한 뷰
+    
+    //1. 데이터 종류?
+    //2. 데이터 갯수
+    //3. (옵션) 데이터 행 눌렀다.
+    
 }
 
